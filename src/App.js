929838babeda167333349fb9cfb328946d2bb65f
leehyelim0691 <qazxcv0691@naver.com>
2021-07-27 11:07:37 +0900

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/styles.css';
+import './css/bootstrap-theme.css';
+import './css/bootstrap-theme.css.map';
+import './css/bootstrap-theme.min.css';
+import './css/bootstrap.css';
+import './css/bootstrap.css.map';
+import './css/bootstrap.min.css';
+import Main from './pages/main';
+
+function App() {
+  return (
+    <Main/>    
+  );
+}
+
+export default App;
---
+++ src/pages/bbs.js
@@ -0,0 +1,9 @@
+export default function bbs() {
+    return(
+        <div className="bbs">
+           <p>
+               This is bbs!
+           </p>
+        </div>
+    );
+}
