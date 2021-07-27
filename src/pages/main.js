ed438a733768b64f22e4ebb4c5c48dd2fb4e2751
22000510 <22000510@handong.edu>
2021-07-27 19:13:41 +0900

    change main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,9 @@
+import 'react-router-dom'
+import BBS from './bbs.js';
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 export default function main() {
     return(
+        
         <div className="main">
             <body id="page-top">
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
@@ -37,8 +41,12 @@ export default function main() {
                         <h2 class="text-white mt-0">BulletinBoard</h2>
                         <hr class="divider divider-light" />
                         <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                        <a class="btn btn-light btn-xl" href="#services">게시글 작성!</a> 
-                        {/* #services가 아닌 게시글 작성하는 새로운 페이지로 갈수있게. */}
+                        <Router>
+                            <Route path="/bbs" component={BBS} />
+                            <Link to="/bbs">
+                            <button>About</button>
+                            </Link>
+                        </Router>
                     </div>
                 </div>
             </div>
